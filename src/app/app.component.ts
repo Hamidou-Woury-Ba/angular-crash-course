c255138d4132e51533e83c143458309cb44f8112
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2025-01-07 00:03:53 +0000

    Ajout d'un component - formulaire - condition (if) - évènement - fonctions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstCompComponent } from "./my-first-comp/my-first-comp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstCompComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-crash-course';
+  title = 'Hamidou application';
 }
---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-my-first-comp',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './my-first-comp.component.html',
+  styleUrl: './my-first-comp.component.scss'
+})
+export class MyFirstCompComponent {
+  name : String = '';
+  email : String = '';
+  message : String = '';
+  isSubmitted : boolean = false;
+
+  onSubmit() : void {
+    this.isSubmitted = true;
+  }
+}
